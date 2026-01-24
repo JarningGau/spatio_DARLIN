87ebecab26b9cabbe0a44d78eb5a7fa9466aa583
JarningGau <jarninggau@gmail.com>
2026-01-24 16:38:51 +0800

    add test

---
+++ test/test_BMKS3000/test_bmk_matlab.sh
@@ -0,0 +1,17 @@
+## Col1a1 Array (CA)
+snakemake -s ../../snakefiles/BMKS3000_matlab.smk \
+--configfile config-CA.yaml \
+--cores 2 \
+--use-conda
+
+## Rosa Array (RA)
+snakemake -s ../../snakefiles/BMKS3000_matlab.smk \
+--configfile config-RA.yaml \
+--cores 2 \
+--use-conda
+
+## Tigre Array (TA)
+snakemake -s ../../snakefiles/BMKS3000_matlab.smk \
+--configfile config-TA.yaml \
+--cores 2 \
+--use-conda
